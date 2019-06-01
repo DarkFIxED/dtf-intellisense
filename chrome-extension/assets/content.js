242325ff4644e51a95ee7437089884e0fc541392
DarkFIxED <fixed.fred@gmail.com>
2019-06-01 21:42:45 +0700

    Added keyboard control

--- chrome-extension/assets/content.js
+++ chrome-extension/assets/content.js
@@ -1,6 +1,80 @@
 const commentsContainerSelector = "#page_wrapper .comments";
 const unobservedCommentsTextAreaSelector = "#page_wrapper .comments textarea.comments_form__textarea:not(.meme-observed)";
 const observedCommentsTextAreaSelector = "#page_wrapper .comments textarea.comments_form__textarea.meme-observed";
+const memeListId = "meme-select";
+const memeListSelector = "#meme-select";
+
+const fakeData = JSON.parse("{  \n" +
+    "   \"isSuccess\":true,\n" +
+    "   \"data\":[  \n" +
+    "      {  \n" +
+    "         \"displayedName\":\"Rick Roll [2]\",\n" +
+    "         \"aliases\":[  \n" +
+    "            \"rick_roll\",\n" +
+    "            \"qgwg\"\n" +
+    "         ]\n" +
+    "      },\n" +
+    "      {  \n" +
+    "         \"displayedName\":\"Дудец\",\n" +
+    "         \"aliases\":[  \n" +
+    "            \"скелет с трубой\",\n" +
+    "            \"дудец\"\n" +
+    "         ]\n" +
+    "      },\n" +
+    "      {  \n" +
+    "         \"displayedName\":\"Hello World\",\n" +
+    "         \"aliases\":[  \n" +
+    "            \"hello_world\"\n" +
+    "         ]\n" +
+    "      },\n" +
+    "      {  \n" +
+    "         \"displayedName\":\"Нихуя не понял, но очень интересно\",\n" +
+    "         \"aliases\":[  \n" +
+    "            \"интересно\",\n" +
+    "            \"нихуя_не_понял\",\n" +
+    "            \"нихуя не понял\"\n" +
+    "         ]\n" +
+    "      }\n" +
+    "   ]\n" +
+    "}");
+
+function buildContent(options) {
+    if (options.length === 0) {
+        return `No matches found`;
+    }
+
+    // language=HTML
+    let result = `<select id="${memeListId}" size="5">`;
+    for (let option of options) {
+        result = result + `<option value="${option.alias}" ${options.indexOf(option) === 0 ? "selected" : ""}>${option.displayingName}&nbsp;(${option.alias})</option>`;
+    }
+
+    result = result + "</select>";
+    return result;
+}
+
+function findMatches(vocabulary, currentValue) {
+    let words = currentValue.split();
+    let matches = [];
+    let lastWord = words[words.length - 1];
+
+    vocabulary
+        .filter(entry => words.length === 0
+            ? true
+            : entry.aliases.some(alias => alias.search(lastWord) >= 0
+            ))
+        .map(entry => entry.aliases.map(alias => {
+            matches.push({
+                alias: alias,
+                displayingName: entry.displayedName
+            });
+        }));
+
+    return {
+        matches: matches,
+        hasRelevantMatches: words.length !== 0 && matches.length > 0
+    };
+}
 
 function processSymbolKey(target, keyboardEvent) {
     let currentValue = target.value;
@@ -8,12 +82,56 @@ function processSymbolKey(target, keyboardEvent) {
         currentValue += keyboardEvent.key;
     }
 
-    setToolTipContent(target, currentValue);
-    openToolTip(target);
+    if (setToolTipContent(target, currentValue).hasRelevantMatches) {
+        openToolTip(target);
+    }
 }
 
 function setToolTipContent(target, value) {
-    $(target).tooltipster('content', value);
+    let matches = findMatches(fakeData.data, value);
+    let content = buildContent(matches.matches);
+    $(target).tooltipster('content', content);
+
+    return {
+        hasRelevantMatches: matches.hasRelevantMatches,
+        optionsCount: content.length
+    };
+}
+
+function selectPrevMatch() {
+    let currentValue = $(memeListSelector).val();
+    let allValues = [];
+
+    let options = $(`${memeListSelector} option`);
+    options.each(option => {
+        allValues.push($(options[option]).attr('value'))
+    });
+
+    let currentIndex = allValues.indexOf(currentValue);
+    let newIndex = currentIndex === 0
+        ? allValues.length - 1
+        : currentIndex - 1;
+
+    let newValue = allValues[newIndex];
+    $(memeListSelector).val(newValue);
+}
+
+function selectNextMatch() {
+    let currentValue = $(memeListSelector).val();
+    let allValues = [];
+
+    let options = $(`${memeListSelector} option`);
+    options.each(option => {
+        allValues.push($(options[option]).attr('value'))
+    });
+
+    let currentIndex = allValues.indexOf(currentValue);
+    let newIndex = currentIndex === allValues.length - 1
+        ? 0
+        : currentIndex + 1;
+
+    let newValue = allValues[newIndex];
+    $(memeListSelector).val(newValue);
 }
 
 function processCommandKey(target, keyboardEvent) {
@@ -28,17 +146,36 @@ function processCommandKey(target, keyboardEvent) {
         let value = target.value.length > 0
             ? target.value.substr(0, target.value.length - 1)
             : target.value;
-        
+
         setToolTipContent(target, value);
     }
 
-    if (keyboardEvent.repeat) {
-        return;
+    if (target['meme'].opened) {
+        // Arrows up and down
+        if (keyboardEvent.keyCode === 38) {
+            selectPrevMatch();
+            return false;
+        }
+
+        // Arrows up and down
+        if (keyboardEvent.keyCode === 40) {
+            selectNextMatch();
+            return false;
+        }
     }
 
+    // Skip repeats
+    if (keyboardEvent.repeat) {
+        return true;
+    }
+
+    // Force show on Ctrl+Space
     if (keyboardEvent.ctrlKey && keyboardEvent.key === " ") {
+        setToolTipContent(target, target.value);
         openToolTip(target);
     }
+
+    return true;
 }
 
 function initInputListener(e) {
@@ -48,7 +185,9 @@ function initInputListener(e) {
     if (keyboardEvent.key.length === 1 && !keyboardEvent.altKey && !keyboardEvent.ctrlKey) {
         processSymbolKey(target, keyboardEvent);
     } else {
-        processCommandKey(target, keyboardEvent);
+        if (!processCommandKey(target, keyboardEvent)) {
+            e.preventDefault();
+        }
     }
 }
 
@@ -71,7 +210,8 @@ function initializeToolTip(textArea) {
         interactive: true,
         selfDestruction: false,
         contentCloning: true,
-        trigger: "custom"
+        trigger: "custom",
+        contentAsHTML: true,
     });
 }
 
@@ -81,7 +221,7 @@ function initializeEventListeners(target) {
     });
 
     $(target).bind("blur", function () {
-        closeToolTip(target);
+        //closeToolTip(target);
     });
 }
 
